2f7f1579c22ee60a490051da685389b12117e0fa
Olenek <vultuc@gmail.com>
2023-12-24 21:23:49 +0300

    callback

--- cinemabot.py
+++ cinemabot.py
@@ -56,7 +56,7 @@ async def movie_not_found(query: CallbackQuery):
 
 
 @dp.callback_query(SearchData.filter(F.movie_id))
-async def send_movie_offers(query: CallbackQuery, data: SearchData):
+async def send_movie_offers(query: CallbackQuery, data: SearchData, bot: Bot):
     await scribe.record_query(query.message.chat.id, query.message.text, data.movie_id, data.movie_nm)
     offers = await searcher.search_offers(query.data, locale_priority=('us/en_us', 'ru/ru'))
     reply_txt = 'Here are some of the places you can watch it:\n'
