7d6d1656b831e940b85dafda4d4b791c098929bc
Olenek <vultuc@gmail.com>
2023-12-25 14:09:36 +0300

    no await where it is not needed

--- src/searcher.py
+++ src/searcher.py
@@ -110,7 +110,7 @@ class Searcher:
         query = locales[locale]['pattern'].format(movie_nm, provider_nm)
         print(query)
         async with self._session.get(
-                url=self._google_search_url, params={'q': query}, header={
+                url=self._google_search_url, params={'q': query}, headers={
                     'Accept': '*/*',
                     'Accept-Language': 'en-US,en;q=0.5',
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82',
