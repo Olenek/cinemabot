39835caea6dfabea691549a22fbcef92403db5ac
Olenek <vultuc@gmail.com>
2023-12-25 14:03:10 +0300

    offers with google search

--- src/searcher.py
+++ src/searcher.py
@@ -1,10 +1,27 @@
-import re
-
-from justwatch import JustWatch
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Iterable, Any
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 
+
+# user_agents = [
+#     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36 OPR/72.0.3815.465 (Edition Yx GX)',
+#     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+#     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+#     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
+#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
+#     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.3 Safari/605.1.15',
+#     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59',
+# ]
+locales = {
+    'RU': {
+        'pattern': '{} смотреть на {}'
+    },
+    'US': {
+        'pattern': '{} watch on {}'
+    }
+}
 class Searcher:
     def __init__(self, tmdb_token: str):
         # self._tmdb_header = {
@@ -15,6 +32,8 @@ class Searcher:
 
         self._tmdb_search_url = 'https://api.themoviedb.org/3/search/movie'
         self._tmdb_watch_providers_url = 'https://api.themoviedb.org/3/movie/{}/watch/providers'
+        self._tmdb_translations_url = 'https://api.themoviedb.org/3/movie/{}/translations'
+        self._google_search_url = 'https://www.google.com/search'
 
         self._session = None
 
@@ -33,35 +52,65 @@ class Searcher:
             return [(item['id'], item['title'], item['release_date'][:4]) for item in response_data[:3]]
         return []
 
-    async def search_offers(self, movie_id: int, locale_priority=('RU', 'EN')):
+    async def search_offers(self, movie_id: int, movie_nm: str) -> Dict[str, str]:
+        """
+        Used to search for streaming offers for the movie
+        :param movie_nm: name of the movie
+        :param movie_id: tmdb movie id
+        :return: Dict[Locale_nm, URL]
+        """
         tmdb_response = await self._session.get(
             self._tmdb_watch_providers_url.format(movie_id),
             params={'api_key': self._tmdb_token},
         )
-        response_data = (await tmdb_response.json())
-        options = response_data['results']
-        any_locale = next(iter(options.keys()))
-        print(any_locale)
+        response_data = await tmdb_response.json()
+        providers = await response_data['results']
+        options = {locale_nm: providers.get(locale_nm, {}) for locale_nm in locales.keys()}
+        return await self._construct_offers(movie_id, movie_nm, options)
 
-        for locale in locale_priority:
-            if options.get(locale, None) is not None:
-                return await self._construct_offer(options[locale]['link'])
-
-        fallback_url = options[any_locale]['link']
-        print(fallback_url)
-        return await self._construct_offer(fallback_url[:10])
-
-    async def _construct_offer(self, tmdb_url: str):
-        print(tmdb_url)
+    async def _get_translated_titles(self, movie_id: int, movie_nm: str):
+        translations: Dict[str, str] = {}
         async with self._session.get(
-            url=tmdb_url
+                url=self._tmdb_translations_url.format(movie_id), params={'api_key': self._tmdb_token},
+        ) as response:
+            json = await response.json()
+            for translation in await json['translations']:
+                if translation['iso_3166_1'] in locales.keys():
+                    if translation['data']['title'] != '':
+                        translations[translation['iso_3166_1']] = translation['data']['title']
+
+        for locale_nm in locales.keys():
+            if locale_nm not in translations.keys():
+                translations[locale_nm] = movie_nm
+
+        return translations
+
+    async def _construct_loc_offer(self, locale_nm: str,
+                                   options: Dict[str, Any],
+                                   translations: Dict[str, str]) -> str | None:
+        watch_variants = ['flatrate', 'rent', 'buy']
+        for variant in watch_variants:
+            for provider_offer in options[variant]:
+                result = await self._try_provider(translations[locale_nm], provider_offer['provider_name'], locale_nm)
+                if result is not None:
+                    return result
+        return None
+
+    async def _construct_offers(self, movie_id: int, movie_nm: str, loc_options: Dict[str, Dict[Any, Any]]) -> Dict[str, str | None]:
+        translations = await self._get_translated_titles(movie_id, movie_nm)
+        offers: Dict[str, str | None] = {}
+        for locale_nm, option in loc_options.items():
+            offers[locale_nm] = await self._construct_loc_offer(locale_nm, option, translations)
+        return offers
+
+    async def _try_provider(self, movie_nm: str, provider_nm: str, locale: str) -> str | None:
+        query = locales[locale].format(movie_nm, provider_nm)
+        async with self._session.get(
+                url=self._google_search_url, params={'q': query},
         ) as response:
             response_text = await response.text()
             soup = BeautifulSoup(response_text, 'html.parser')
-            print(soup)
-            section = soup.find('div', {'class': 'ott_provider'})
-            print(section)
-            if section is None:
-                return []
-            print(section.find_all('a', {'class': lambda x: str.startswith(x, 'Watch')}))
-            return []
+            first_link = soup.find(id='search').find('a')
+            if first_link.get('href', None) is not None:
+                return first_link['href']
+            return None
